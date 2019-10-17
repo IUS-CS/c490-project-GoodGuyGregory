82d650b04e3f6a7c65c75068bd1f28829ff12771
GoodGuyGregory <greg.witt625@gmail.com>
2019-10-17 14:57:34 -0400

    initial commit for project

---
+++ MusicTrainer/app/src/main/java/goodguygregory/app/musictrainer/MainActivity.kt
@@ -0,0 +1,12 @@
+package goodguygregory.app.musictrainer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
