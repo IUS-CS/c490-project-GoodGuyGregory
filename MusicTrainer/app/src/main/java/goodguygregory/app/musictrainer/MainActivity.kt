5bf0d5d3256f967c88b7908d3e979e6e824784d2
GoodGuyGregory <greg.witt625@gmail.com>
2019-11-12 01:22:56 -0500

    adds medium chords and chord progression generation

--- MusicTrainer/app/src/main/java/goodguygregory/app/musictrainer/MainActivity.kt
+++ MusicTrainer/app/src/main/java/goodguygregory/app/musictrainer/MainActivity.kt
@@ -45,7 +45,25 @@ class MainActivity : AppCompatActivity() {
             "I - vi - IV -V"
         )
 //        mediumProgressions
-//        val mediumChords = arrayOf()
+        val mediumChords = arrayOf(
+            "I - iv - V7 - I",
+            "I - I - V -V -I6 - IV - V7 - I",
+            "I - V - V7 - I I6 - IV - V7 - I",
+            "I - III7 - IV - V7",
+            "i - VI - iv - vii*",
+            "I - V - vi - iii - IV - I - ii6/5 - V7",
+            "i - iv - V - i - IV - ii*6 - V - i",
+            "I - IV - V - I - vi - ii6 - V - I",
+            "I - IV - V - I - vi - ii6 - V - I",
+            "i - V - VI - III - iv - i - ii6/5 - V7",
+            "i - V - V7 - i i6 - iv - V7 - i",
+            "i - VI - III - vii*",
+            "I - IV - V - V7 - I - IV - V7 - I",
+            "bVI - bIII - bVIII - IV - I - I - I - I",
+            "ii* - V7 - i - i",
+            "i - iv - VII - III - VI - ii* - V - i",
+            "I - vi - ii - V7"
+        )
 
 //        hardProgressions
 //        val hardChords = arrayOf()
@@ -63,6 +81,15 @@ class MainActivity : AppCompatActivity() {
             keySignatureValue!!.text = easyKey
 
 //       generates random progression
+            val easyProgressionGenerator = Random()
+
+//            generate random progression
+            val randomEasyProg = easyProgressionGenerator.nextInt(easyChords.size)
+
+            val easyProg = easyChords[randomEasyProg]
+
+            progressionTextView!!.text = easyProg
+
 
         }
     }
